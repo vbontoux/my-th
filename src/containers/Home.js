b168480a7212d1f7a6a314f9fb7479e19cc24bd9
Thibaut Cens <thibaut.cens@gmail.com>
2018-10-16 13:35:29 -0500

    Updated some id

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,7 +5,7 @@ import "../styles/wrappers.css"
 export default class Home extends Component{
     render() {
         return (
-            <div className="Home">
+            <div id="Home">
                 <div className="test_text">
                     <h1>My-TreasureHunt</h1>
                     <p> Fun bots to enrich your visuals
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,14 +1,11 @@
 import React, {Component} from "react"
 
-import Icon from "@mdi/react"
-import {mdiShovel} from "@mdi/js"
-
 import "../styles/wrappers.css"
 
 export default class Login extends Component{
     render() {
         return (
-            <div className="Home">
+            <div className="Login">
                 <div className="test_text">
                     <h1>Login</h1>
                     <p>Some work goes here</p>
